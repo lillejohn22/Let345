123609394bd12a2d90571947d1526ca6f93060a9
Anton Frigård <antonfrigard@icloud.com>
2018-04-06 13:30:08 +0200

    Added new comparator

---
+++ LET375_lab_1/src/ReverseStringComparator.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+import java.util.Set;
+
+public class ReverseStringComparator implements Comparator<String>{
+	public int compare(String s1, String s2) {
+		
+		return -1;
+	}
+	
+
+}
