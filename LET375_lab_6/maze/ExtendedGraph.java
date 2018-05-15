245433826757c9078393946a932c62b6bc5e33af
Johan <nilssojo@student.chalmers.se>
2018-05-15 17:34:47 +0200

    test 2

--- LET375_lab_6/maze/ExtendedGraph.java
+++ LET375_lab_6/maze/ExtendedGraph.java
@@ -3,20 +3,23 @@ import java.util.*;
 
 
 public class ExtendedGraph extends Graph {
-	
+	int count = 0; 
 	List<Integer> nodePath;
 	List<Integer> tempPath;
 	
 	public List<Integer> getPath( int destName ) {
 		nodePath = new ArrayList<Integer>();
 		nodePath = getPath(vertexMap.get( destName ));
-		
+		for(Integer noden : nodePath) {
+			System.out.println(noden);
+		}
 		
 		return nodePath;
 	}
 	
 	private List<Integer> getPath( Vertex dest ) {
 		ArrayList<Integer> node = new ArrayList<Integer>();
+		
 		if(dest.prev != null) {
 			node.addAll(getPath(dest.prev));
 			return node;
